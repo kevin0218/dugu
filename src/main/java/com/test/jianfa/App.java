fb174378c46a3a9a17f4f9f15f3d5265170e77a4
zxgtopsec <zhou_xingguo@topsec.com.cn>
2019-09-26 17:20:53 +0800

    Initial commit

---
+++ src/main/java/com/test/jianfa/App.java
@@ -0,0 +1,14 @@
+package com.test.jianfa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("生活总有不如意！！！！！！！！！！！！！！！！");
+    }
+}
